e4a433f94d79d9ed1468246932bbb911ed313f65
Ruslan Pantaev <rpantaev@gmail.com>
2018-05-13 22:17:58 -0400

    hw9 complete

--- hw9/hw9.java
+++ hw9/hw9.java
@@ -24,11 +24,132 @@
  * @author  Ruslan Pantaev
  */
 
+// 1) For the first part, Object class was probably extracted somewhere
+// where it wasn't accessible. Not sure of the specifics, but definitely
+// a sys-specific bug.
+// 
+// 2) It appears that the calls to e.producer() and e.consumer() are throwing exception(s)
+// that are not being displayed to console.
+// 
+// 3) After wrapping notifyAll() in try/catch blocks, I saw that e.producer() actually ran
+// the notifyAll() method, but that a thrown exception was caught in e.consumer().
+// 
+// After debugging w/ print statements, I simply wrote:
+//  try {
+//      notifyAll();
+// } catch(IllegalMonitorStateException i){}
+// 
+// Now it appears to run.
+// 
+// 4) Found this re: Threads and Object's monitor
+// 
+// "Each object has an 'Object monitor'. Basically it is a 'semaphore',
+// indicating if a critical section code is being executed by a thread or not.
+// Before a critical section can be executed, the Thread must obtain an
+// 'Object monitor'. Only one Thread at a time can own that object's monitor."
+// -ref https://en.wikibooks.org/wiki/Java_Programming/API/java.lang.Object#Synchronizing_Threads_Methods
+// 
+// At any given time, one of the two threads attempted to call notifyAll() on an object monitor without permissions
+// of said monitor. Basically, a critical section cannot be executed by thread until that thread owns the object's
+// monitor.
+
 import java.util.*;
-
-public class hw9 {
-
-    public static void main(String[] args) {
+import java.io.*;
+import java.net.*;
+ 
+public class hw9{
+ 
+    public static void main(String[] args) throws Throwable{
+        economy myeconomy = new economy();
+ 
+        Thread t1 = new Thread(new producer(myeconomy));
+        Thread t2 = new Thread(new consumer(myeconomy));
         
+        t2.start();
+        t1.start();
+    }
+}
+ 
+class producer implements Runnable{
+    economy e;
+ 
+    producer(economy e){
+        this.e = e;
+    }
+ 
+    public void run(){
+        try{
+            e.producer();
+        }catch(Throwable t){}
+    }
+}
+ 
+class consumer implements Runnable{
+    economy e;
+ 
+    consumer(economy e){
+        this.e = e;
+    }
+ 
+    public void run(){
+        try{
+            e.consumer();
+        }catch(Throwable t){}
+    }
+}
+ 
+class economy{
+    boolean product = false;
+ 
+    public void producer() throws Throwable{
+        System.out.println("Running producer");
+        while(true){
+            if(product == true){
+                try{
+                    System.out.println("Producer: Too much product exists. Gonna wait...");
+                    wait();
+                }catch(Throwable t){}
+            }
+            System.out.println("Producer: Making more product now.");
+            product = true;
+            try {
+                notifyAll();
+            // }catch(Throwable t){}
+            } catch(IllegalMonitorStateException i) {
+                // System.out.println("IllegalMonitorStateException in producer!");
+            }
+            // finally {
+            //     System.out.println("Finally block in producer...");
+            // }
+            Thread.sleep(1000);
+            System.out.println("Producer ending");
+ 
+        }
+    }
+ 
+    public void consumer() throws Throwable{
+        System.out.println("Running Consumer");
+        while(true){
+            if(product == false){
+                try{
+                    System.out.println("Consumer: No products available to consume. Gonna wait...");
+                    wait();
+                }catch(Throwable t){}
+            }
+ 
+            System.out.println("Consumer: Now consuming product");
+            product = false;
+            try {
+                notifyAll();
+            // }catch(Throwable t){}
+            } catch(IllegalMonitorStateException i){
+                // System.out.println("IllegalMonitorStateException in consumer!");
+            }
+            // finally {
+            //     System.out.println("Finally block in consumer...");
+            // }
+            Thread.sleep(10);
+            System.out.println("Consumer ending");
+        }
     }
 }
